1554e3387ed222b65cea2d6bfcf6bb42cf5dd1d8
leonli <leonli@wezhuiyi.com>
2023-03-09 16:40:56 +0800

    feat: init

---
+++ pages/index/index.vue
@@ -0,0 +1,17 @@
+<template>
+	<view class="container">
+			{{ title }}
+	</view>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const title = ref('---')
+
+</script>
+
+<style>
+	.container {
+		font-size: 100px;
+	}
+</style>
---
+++ pages/user/index/index.vue
@@ -0,0 +1,21 @@
+<template>
+	<view class="user-block">
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+.user-block {
+	font-size: 10px;
+}
+</style>
