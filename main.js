ceea0b7d2204dde9b594e5f49ddb80319697efb3
leonli <leonli@wezhuiyi.com>
2023-04-11 18:23:58 +0800

    feat: 新增依赖uniui

--- main.js
+++ main.js
@@ -3,8 +3,6 @@ import { createSSRApp } from 'vue'
 import store from './store'
 import './assets/iconfont.css'
 
-
-
 export function createApp() {
   const app = createSSRApp(App)
   app.use(store)
--- unpackage/dist/dev/mp-weixin/pages/index/index.js
+++ unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -1,5 +1,6 @@
 "use strict";
 const common_vendor = require("../../common/vendor.js");
+const common_assets = require("../../common/assets.js");
 const api_category = require("../../api/category.js");
 require("../../api/base.js");
 require("../../store/index.js");
@@ -7,18 +8,20 @@ require("../../constans/index.js");
 require("../../api/user.js");
 require("../../store/modules/topic.js");
 require("../../api/topic.js");
+if (!Array) {
+  const _easycom_uni_badge2 = common_vendor.resolveComponent("uni-badge");
+  _easycom_uni_badge2();
+}
+const _easycom_uni_badge = () => "../../node-modules/@dcloudio/uni-ui/lib/uni-badge/uni-badge.js";
+if (!Math) {
+  (_easycom_uni_badge + fuiCol + fuiRow)();
+}
+const fuiRow = () => "../../components/firstui/fui-row/fui-row.js";
+const fuiCol = () => "../../components/firstui/fui-col/fui-col.js";
 const _sfc_main = {
   __name: "index",
   setup(__props) {
     const isRefresher = common_vendor.ref(false);
-    const carouselList = common_vendor.ref([
-      {
-        image: "https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg"
-      },
-      {
-        image: "https://fuss10.elemecdn.com/2/11/6535bcfb26e4c79b48ddde44f4b6fjpeg.jpeg"
-      }
-    ]);
     const data = common_vendor.ref([
       {
         label: "前端",
@@ -42,9 +45,6 @@ const _sfc_main = {
     const category = common_vendor.computed(() => {
       return data.value.filter((_) => _.child.length > 1);
     });
-    const navTo = (item) => {
-      console.log(item);
-    };
     const handleSelect = (main, child) => {
       common_vendor.index.navigateTo({
         url: `/pages/topic/index?categoryId=${child.categoryId}&title=${child.categoryName}`
@@ -65,34 +65,45 @@ const _sfc_main = {
     };
     return (_ctx, _cache) => {
       return {
-        a: common_vendor.f(carouselList.value, (item, index, i0) => {
-          return {
-            a: item.image,
-            b: item.type,
-            c: common_vendor.o(($event) => navTo(item), item.type)
-          };
+        a: common_assets._imports_0,
+        b: common_vendor.p({
+          text: "1"
         }),
-        b: common_vendor.f(common_vendor.unref(category), (item, index, i0) => {
+        c: common_vendor.p({
+          text: "2",
+          type: "success"
+        }),
+        d: common_vendor.p({
+          text: "3",
+          type: "primary",
+          inverted: true
+        }),
+        e: common_vendor.f(common_vendor.unref(category), (item, index, i0) => {
           return {
-            a: common_vendor.t(item.label),
-            b: common_vendor.f(item.child, (child, child_index, i1) => {
+            a: common_vendor.f(item.child, (child, child_index, i1) => {
               return {
                 a: common_vendor.n(`iconfont ${child.icon}`),
                 b: common_vendor.t(child.categoryName),
-                c: child.categoryId,
-                d: common_vendor.o(($event) => handleSelect(item, child), child.categoryId)
+                c: common_vendor.t(child.count),
+                d: child.categoryId,
+                e: common_vendor.o(($event) => handleSelect(item, child), child.categoryId),
+                f: "1cf27b2a-4-" + i0 + "-" + i1 + "," + ("1cf27b2a-3-" + i0)
               };
             }),
+            b: "1cf27b2a-3-" + i0,
             c: index
           };
         }),
-        c: isRefresher.value,
-        d: common_vendor.o(onPulling),
-        e: common_vendor.o(onRefresh),
-        f: common_vendor.o(onRestore)
+        f: common_vendor.p({
+          span: 12
+        }),
+        g: isRefresher.value,
+        h: common_vendor.o(onPulling),
+        i: common_vendor.o(onRefresh),
+        j: common_vendor.o(onRestore)
       };
     };
   }
 };
-const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["__file", "/Users/lichunlin/code/gitHub/face/pages/index/index.vue"]]);
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["__scopeId", "data-v-1cf27b2a"], ["__file", "/Users/lichunlin/code/gitHub/face/pages/index/index.vue"]]);
 wx.createPage(MiniProgramPage);
